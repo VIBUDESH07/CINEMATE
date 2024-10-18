8c849cf5a84cd6fe78b548e696bb98d682df1fc6
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-10-18 16:16:37 +0530

    Update App.js

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import HomePage from './components/Home/Home'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/Home/Home.jsx';
+import MovieDetails from './components/Home/MovieDetails.jsx';
+import LoginSignup from './components/Home/Login.jsx';
+import FilterPage from './components/Home/FilterPage.jsx';
+import MoviesPage from './components/Home/MoviesPage.jsx';
 
 const App = () => {
   return (
-    <div>
-      <HomePage/>
-    </div>
-  )
-}
+      <Routes>
+        {/* Define your routes here */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginSignup />} /> {/* Home Page route */}
+        <Route path="/movie/:id" element={<MovieDetails />} /> 
+        <Route path="/filter" element={<FilterPage />} />
+        <Route path="/movies" element={<MoviesPage />} /> {/* Movie details route */}
+      </Routes>
+    
+  );
+};
 
-export default App
+export default App;
